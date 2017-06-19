efba2f044cc7fb017dbbc286d83ae33ddc9dfddc
Ing. Miroslav Biňas PhD <miroslav.binas@tuke.sk>
2017-06-19 07:31:15 +0200

    zrusenie id v serializerovi

--- dbank/account/serializers.py
+++ dbank/account/serializers.py
@@ -4,12 +4,6 @@ from .models import Account
 
 
 class AccountSerializer(serializers.ModelSerializer):
-    id = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='accounts_detail'
-    )
-
     class Meta:
         model = Account
         fields = ('id', 'iban', 'balance',)
