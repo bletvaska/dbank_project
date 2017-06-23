71bd541fdf978196ad5b680eeb5de07d6cb52771
Ing. Miroslav Biňas PhD <miroslav.binas@tuke.sk>
2017-06-23 08:14:58 +0200

    fix url

--- dbank/account/urls.py
+++ dbank/account/urls.py
@@ -5,7 +5,7 @@ from .views import AccountListView, \
     TransactionListView, AccountCreate, TransactionCreate, DashboardView, \
     AccountViewSet, ClientViewSet, TransactionViewSet
 
-# app_name = 'bank'
+app_name = 'bank'
 
 router = routers.DefaultRouter()
 router.register(r'api/accounts', AccountViewSet, base_name='account')
@@ -21,10 +21,10 @@ urlpatterns += [
 
     url(r'dashboard/$', DashboardView.as_view(), name='dashboard'),
 
-    # url(r'accounts/$', AccountListView.as_view(), name='accounts_list'),
-    # url(r'accounts/create$', AccountCreate.as_view(), name='accounts_create'),
-    # url(r'accounts/(?P<account_id>\d+)/transactions$', TransactionListView.as_view(), name='account_transactions'),
-    # url(r'transactions/$', TransactionListView.as_view(), name='transactions_list'),
-    # url(r'transactions/create$', TransactionCreate.as_view(), name='transactions_create'),
+    url(r'accounts/$', AccountListView.as_view(), name='accounts_list'),
+    url(r'accounts/create$', AccountCreate.as_view(), name='accounts_create'),
+    url(r'accounts/(?P<account_id>\d+)/transactions$', TransactionListView.as_view(), name='account_transactions'),
+    url(r'transactions/$', TransactionListView.as_view(), name='transactions_list'),
+    url(r'transactions/create$', TransactionCreate.as_view(), name='transactions_create'),
 ]
 
